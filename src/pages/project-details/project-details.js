98d3da4db419ed6350c1b5d113284287bc328924
Artsiom <Ar.rakitski@gmail.com>
2023-06-28 17:01:26 +0200

    feat: Added project reducer and created route for project page
    - Implemented a project reducer to manage state related to projects
    - Created actions and reducers to handle project data manipulation
    - Integrated the project reducer into the application's Redux store
    - Added a route for the project page to allow viewing individual project details

--- src/pages/project-details/project-details.js
+++ src/pages/project-details/project-details.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ImageCard from '../../components/image-card'
 import styled from 'styled-components'
+import { useSelector } from 'react-redux'
+import { project } from '../../project/projectSlice'
 
 const Title = styled.h1`
     font-size: 2em;
@@ -41,18 +43,8 @@ const Description = styled(Article)`
 `
 
 const Project = () => {
-    const proj = {
-        "name": "Hamburg",
-        "location": "Hamburg",
-        "year": 2023,
-        "type": ["Elektromontage", "Montage von Lagerregalen"],
-        "img": [
-            '/img/factory1.jpg',
-            '/img/factory2.jpg',
-            '/img/factory3.jpg'
-        ],
-        "desc": "Unsere durchgeführten Arbeiten umfassten die Montage von Lagerregalen in einem neuen Lagergebäude sowie die Installation von Beleuchtungssteckdosen und Lichtschaltern in diesem neuen Bereich. Für die Montage der Lagerregale haben wir unser erfahrenes Team von Monteuren eingesetzt, die mit großer Sorgfalt und Präzision die Regale gemäß den spezifischen Anforderungen des Kunden aufgebaut haben. Die Montage erfolgte effizient und gewährleistete eine optimale Raumnutzung für die Lagerung verschiedener Produkte. Darüber hinaus haben wir auch die elektrische Infrastruktur des neuen Lagerbereichs installiert. Dies beinhaltete die fachgerechte Montage von Steckdosen für die Beleuchtung sowie die korrekte Positionierung der Lichtschalter, um eine komfortable und praktische Nutzung des Lagerbereichs zu gewährleisten. Unser Team arbeitete eng mit dem Kunden zusammen, um sicherzustellen, dass die Montage der Lagerregale und die Installation der elektrischen Komponenten den individuellen Anforderungen und Standards entsprachen. Mit unserer Fachkenntnis und unserem Engagement haben wir sichergestellt, dass das neue Lagergebäude optimal ausgestattet ist, um den Bedürfnissen des Kunden gerecht zu werden"
-    }
+    const proj = useSelector(project)
+
     return (
         <section>
             <Article>
@@ -71,7 +63,7 @@ const Project = () => {
             <ImageCard images={proj.img} src={proj.img[0]} alt={proj.name} />
             <Description>
                 <AboutTitle>
-                    About project
+                    About the project
                 </AboutTitle>
                 <p>{proj.desc}</p>
             </Description>
--- src/pages/projects/object-preview.js
+++ src/pages/projects/object-preview.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import styled, { keyframes } from 'styled-components'
 
 const growing = keyframes`
@@ -11,11 +12,13 @@ const growing = keyframes`
     }
 `
 
-const StyledPreview = styled.div`
+const StyledPreview = styled(NavLink)`
     display: flex;
     flex-direction: row;
     padding: 3em;
     border-radius: 1em;
+    text-decoration: none;
+    color: inherit;
     &:hover {
         transition: all 1s linear;
         background-color: #d8d7d770;
@@ -52,7 +55,7 @@ const PreviewDesc = styled.p`
 const ObjectPreview = ({ project }) => {
     const { name, location, preview_desc, img } = project
     return (
-        <StyledPreview>
+        <StyledPreview to={`/project/${project.id}`}>
             <Desc>
                 <h4>{name}</h4>
                 {location}
