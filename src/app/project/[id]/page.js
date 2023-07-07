1060fb08fea24e0813823fdfa1c614d4367579df
Artem Rakitski <Ar.rakitski@gmail.com>
2023-07-07 10:52:14 +0200

    fix: fix request url

--- src/app/project/[id]/page.js
+++ src/app/project/[id]/page.js
@@ -5,7 +5,7 @@ import Tag from '@/components/tag'
 import Header from '@/components/header/header'
 
 export const getProject = async (id) => {
-    const URL = process.env.VERCEL_URL || process.env.URL;
+    const URL = `https://${process.env.VERCEL_URL}` || process.env.URL;
     try {
         const res = await fetch(`${URL}/api/project/${id}`)
         return res.json()
